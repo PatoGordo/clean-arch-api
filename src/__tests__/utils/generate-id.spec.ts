6938c8e0740ed9f699dac18d7119a3ac15bad357
PatoGordo <conversecomicaro@gmail.com>
2023-06-13 23:53:14 -0300

    feat: added generate-id utility

---
+++ src/__tests__/utils/generate-id.spec.ts
@@ -0,0 +1,15 @@
+import { generateID } from "../../utils/generate-id";
+
+describe("Generate ID Testing", () => {
+  it("Should to generate an 8 characters length ID", () => {
+    const id = generateID(8);
+
+    expect(id.length).toBe(8);
+  });
+
+  it("Should to generate an 8 characters length ID using A letter only", () => {
+    const id = generateID(8, "A");
+
+    expect(id).toEqual("AAAAAAAA");
+  });
+});
