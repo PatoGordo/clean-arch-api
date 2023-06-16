5701b28e5be97e310f1d920b9d7893b32411b4b3
IcaroMiguel <conversecomicaro@gmail.com>
2023-06-16 15:20:34 -0300

    test: remove express integration test

--- src/infra/http/routes/auth.router.ts
+++ src/infra/http/routes/auth.router.ts
@@ -4,9 +4,4 @@ import { SignInUseCase } from "../../../application/modules/auth/sign-in/sign-in
 
 const authRouter = Router();
 
-authRouter.post(
-  "/sign-in",
-  new SignInControler(new SignInUseCase(1 as any)).handler,
-);
-
 export { authRouter };
