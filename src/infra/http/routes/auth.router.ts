f8da0f29bf6d5f7cab49b23974aa7540a7feb103
IcaroMiguel <conversecomicaro@gmail.com>
2023-07-19 09:18:35 -0300

    feat: implement modules in the routers

--- src/infra/http/routes/auth.router.ts
+++ src/infra/http/routes/auth.router.ts
@@ -1,7 +1,16 @@
 import { Router } from "express";
-import { SignInControler } from "../../../application/modules/auth/sign-in/sign-in.controller";
-import { SignInUseCase } from "../../../application/modules/auth/sign-in/sign-in.usecase";
+import { SignInModule } from "../../../application/modules/auth/sign-in/sign-in.module";
+import { InMemoryAuthRepository } from "../../database/in-memory/repositories/in-memory-auth.repository";
+import { SignUpModule } from "../../../application/modules/auth/sign-up/sign-up.module";
+
+const authRepository = new InMemoryAuthRepository();
+
+const signIn = new SignInModule(authRepository);
+const signUp = new SignUpModule(authRepository);
 
 const authRouter = Router();
+
+authRouter.post("/sign-in", signIn.execute().handler);
+authRouter.post("/sign-up", signUp.execute().handler);
 
 export { authRouter };
