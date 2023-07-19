bc69995c4a7e6e56acf83fd7ec5271e1e0b0c4eb
IcaroMiguel <conversecomicaro@gmail.com>
2023-07-19 11:54:46 -0300

    refactor: change the Module class basic structure

--- src/application/adapters/http/module.ts
+++ src/application/adapters/http/module.ts
@@ -1,7 +1,21 @@
 import { Controller } from "./controller";
+import { UseCase } from "./usecase";
 
-export abstract class Module<T> {
+export class Module<T> {
+  controller?: any;
+  useCase?: any;
+
   constructor(protected repository: T) {}
 
-  abstract execute(): Controller<any>;
+  execute(): Controller<any> {
+    if (!this.useCase) {
+      throw new Error("Use case ins't defined");
+    }
+
+    const useCase = new this.useCase(this.repository);
+
+    const controller = new this.controller(useCase);
+
+    return controller;
+  }
 }
