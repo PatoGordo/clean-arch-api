8a296c9a2595c074a2f25a12936e0a04b829f8e6
IcaroMiguel <conversecomicaro@gmail.com>
2023-07-19 11:51:44 -0300

    refactor: change the module that manage the sessions flow

--- src/application/modules/session/end-session/end-session.controller.ts
+++ src/application/modules/session/end-session/end-session.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from "../../../adapters/http/controller";
 import { EndSessionUseCase } from "./end-session.usecase";
 
-export class EndSessionControler extends Controller<EndSessionUseCase> {
+export class EndSessionController extends Controller<EndSessionUseCase> {
   public execute() {
     return this.handleResult({});
   }
--- src/application/modules/session/find-sessions/find-sessions.controller.ts
+++ src/application/modules/session/find-sessions/find-sessions.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from "../../../adapters/http/controller";
 import { FindSessionsUseCase } from "./find-sessions.usecase";
 
-export class FindSessionsControler extends Controller<FindSessionsUseCase> {
+export class FindSessionsController extends Controller<FindSessionsUseCase> {
   public execute() {
     return this.handleResult({});
   }
